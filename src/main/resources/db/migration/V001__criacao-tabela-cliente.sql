6cb12c8987c924cb2ee47803b09420a0ae0e1d37
Marcelo Lins <marcelolinsdemedeiros@gmail.com>
2022-05-17 00:05:54 -0300

    Correção tabela Cliente

--- src/main/resources/db/migration/V001__criacao-tabela-cliente.sql
+++ src/main/resources/db/migration/V001__criacao-tabela-cliente.sql
@@ -1,7 +1,7 @@
 create table cliente (
 	id integer not null auto_increment,
     nome varchar(60) not null,
-    cnpjCpf varchar(11) not null,
+    cnpj_cpf varchar(11) not null,
 
     primary key(id)
     ) engine=InnoDB default charset=utf8;
