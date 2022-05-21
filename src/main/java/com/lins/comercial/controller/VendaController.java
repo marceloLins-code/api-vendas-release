6a87dc149c6a6b776b8fc613c66b59bc0142c0a2
Marcelo Lins <marcelolinsdemedeiros@gmail.com>
2022-05-20 22:31:59 -0300

    Adicionando paginaçoes padão a lista de vendas

--- src/main/java/com/lins/comercial/controller/VendaController.java
+++ src/main/java/com/lins/comercial/controller/VendaController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -29,8 +31,8 @@ public class VendaController {
 	private VendaRepository vendaRepository;
 
 	@GetMapping
-	public List<Venda> listar() {
-		return vendaRepository.findAll();
+	public Page<Venda> listar(Pageable pageable) {
+		return vendaRepository.findAll(pageable);
 	}
 
 	@GetMapping("/{vendaId}")
